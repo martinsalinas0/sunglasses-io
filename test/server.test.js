f16fe2d700aed518b967723fd130d4269686589b
martinsalinas0 <martinlsalinas@pm.me>
2025-05-06 21:50:43 -0500

    2 test passing

--- test/server.test.js
+++ test/server.test.js
@@ -1,26 +1,44 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
+const chai = require("chai"); 
+const chaiHttp = require("chai-http"); 
+const server = require("../app/server"); 
 
-const should = chai.should();
-chai.use(chaiHttp);
+const should = chai.should(); 
+chai.use(chaiHttp); 
 
-// TODO: Write tests for the server
+let token; 
 
 
-///api/brands
-describe('Brands', () => {
-  it('should get all brands', (done) => {
-    chai.request(server)
-      .get('/api/brands')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.an('object');
-        res.body.should.have.property('brands');
-        res.body.brands.should.be.an('array'); 
-        
-        done();
-      });
-  });
-});
 
+//GET brands
+describe("GET /api/brands - get all brands", () => { 
+  it("should get all brands", (done) => { 
+    chai
+      .request(server)
+      .get("/api/brands")
+      .end((err, res) => { 
+        res.should.have.status(200); 
+        res.body.should.be.an("object");
+        res.body.should.have.property("brands");
+        res.body.brands.should.be.an("array");
+        done();  
+      
+      })
+  })
+})
+
+
+//GET products
+describe("GET /api/products - should get all products", () => { 
+  it("should get all products", (done) => { 
+    chai
+    .request(server)
+    .get("/api/products")
+    .end((err, res) => { 
+      res.should.have.status(200); 
+      res.body.should.be.an("object");
+      res.body.should.have.property("products");
+      res.body.products.should.be.an("array");
+      done();  
+    })
+  })
+})
