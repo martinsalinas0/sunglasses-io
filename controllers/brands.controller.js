fe99e315dfb61ced7461660253723709486b807b
martinsalinas0 <martinlsalinas@pm.me>
2025-05-07 21:56:52 -0500

    brands.contorller

--- controllers/brands.controller.js
+++ controllers/brands.controller.js
@@ -1,16 +1,11 @@
-const getBrands = (req, res) => { 
-const brands = require('../initial-data/brands.json')
+const brands = require("../initial-data/brands.json");
 
-try {
-  res.status(200).json({brands: brands})
-} catch (error) {
-  res.status(500).json({message: error.message})
-}
+const getBrands = (req, res) => {
+  try {
+    res.status(200).json({ brands: brands });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-
-}
-
-
-
-module.exports = {getBrands }
-
+module.exports = { getBrands };
