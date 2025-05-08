dfcaf60b1e8f1b6b5197e64a1c423ca912a24c10
martinsalinas0 <martinlsalinas@pm.me>
2025-05-07 21:48:09 -0500

    add product controller

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,8 +1,22 @@
-const getProducts = (req, res) => {
-  const products = require("../initial-data/products.json");
+const products = require("../initial-data/products.json");
 
+const getProducts = (req, res) => {
   try {
     res.status(200).json({ products: products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProductsByBrand = (req, res) => {
+  try {
+    const { brandId } = req.params;
+
+    const filterBrands = products.filter((product) => {
+      product.brandId === brandId;
+      
+    });
+    res.status(200).json({filterBrands})
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
